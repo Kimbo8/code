b67403b6044c9a5cfb68576a5eecb41ecf2b2a7f
Kimbo8 <117293024+Kimbo8@users.noreply.github.com>
2022-11-04 10:03:46 -0600

    Create midtermpt2.py

---
+++ midtermpt2.py
@@ -0,0 +1,32 @@
+#4
+#num = 20
+#while num <=60:
+ #   print(num)
+ #   num+=2
+#-----------------------
+#5
+#quit = False
+#while quit == False:
+  #  password=input("press enter")
+   # while password != '12345':
+    #    password = input("enter password")
+  #  else:
+  #      quit = True
+#------------------------------------------
+#6
+#def pizza(cheese,crust,tomatoSauce,pepperoni):
+  #  return cheese+crust-tomatoSauce+pepperoni
+
+#print(pizza(10,20,30,40))
+#------------------------------------------------
+#7
+def garden(seeds):
+    print("the pumpkins are growing")
+    while seeds > 0:
+       
+        print("Now you have", seeds, "pumpkins")
+        seeds+=1
+num = int(input("Enter the number of seeds youve planted:"))
+garden(num)
+#cant stop the loop
+   
