06904f6c1e160495fdc60b5f59252ca95105237e
Kimbo8 <117293024+Kimbo8@users.noreply.github.com>
2023-03-10 09:04:47 -0700

    Create good ship.py

---
+++ good ship.py
@@ -0,0 +1,27 @@
+import sys
+import math
+import string
+
+
+# Always start with reading in the number
+#of test cases gtom standard input. The rstricp() methof removes the lingerin newline
+cases = int(sys.stdin.readline().rstrip())
+
+for caseNum in range(cases):
+    
+    
+    dataString = sys.stdin.readline()
+    data = dataString.split(" ")
+    
+   
+    shipyardDataLength = int(data[0])
+    shipDataLength = float(data[1])
+    
+    
+    shipyardData = []
+    
+    
+    for i in range(len(shipyardDataLength)):
+        shipyardData[x] = sys.stdin.readline()
+    for i in range(len(shipyardDataLength)):
+        shipData[i] = sys/stdin.readline()
